8f765c0000f29c32a76983f48709556fd259d746
yu000189 <yu000189@algonquinlive.com>
2019-01-21 20:08:47 -0500

    lab2

--- app/src/main/java/com/example/androidlabs/MainActivity.java
+++ app/src/main/java/com/example/androidlabs/MainActivity.java
@@ -8,6 +8,8 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main_grid);
+        //setContentView(R.layout.activity_main_linear);
+        //setContentView(R.layout.activity_main_grid);
+        setContentView(R.layout.activity_main_relative);
     }
 }
